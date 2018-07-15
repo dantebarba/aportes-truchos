2c7c23be44437a0f8d0bf23e92b1d9a5bd5d914c
Dante Barba <dantebarba@gmail.com>
2018-07-15 17:57:29 -0300

    added forwarded keys for reverse and nat resolving.

--- aportes-truchos-api/src/main/java/com/github/dantebarba/aportestruchos/api/RequestLoggerInterceptor.java
+++ aportes-truchos-api/src/main/java/com/github/dantebarba/aportestruchos/api/RequestLoggerInterceptor.java
@@ -27,10 +27,23 @@ public class RequestLoggerInterceptor implements ContainerRequestFilter {
 
 	@Override
 	public void filter(ContainerRequestContext ctx) throws IOException {
+		RequestDto dto = getRequestDto();
+		logging.log(servletRequest.getRemoteAddr(), new Gson().toJson(dto));
+	}
+
+	private RequestDto getRequestDto() {
 		RequestDto dto = new RequestDto();
 		dto.url = servletRequest.getRequestURL().toString();
 		dto.uri = servletRequest.getRequestURI();
 		dto.os = servletRequest.getHeader("User-Agent");
-		logging.log(servletRequest.getRemoteAddr(), new Gson().toJson(dto));
+		dto.xforwardedfor = servletRequest.getHeader("X-FORWARDED-FOR");
+		dto.httpxforwardedfor = servletRequest.getHeader("HTTP_X_FORWARDED_FOR");
+		dto.httpxforwarded = servletRequest.getHeader("HTTP_X_FORWARDED");
+		dto.httpclusterclientip = servletRequest.getHeader("HTTP_X_CLUSTER_CLIENT_IP");
+		dto.httpclientip = servletRequest.getHeader("HTTP_CLIENT_IP");
+		dto.httpforwardedfor = servletRequest.getHeader("HTTP_FORWARDED_FOR");
+		dto.httpforwarded = servletRequest.getHeader("HTTP_FORWARDED");
+		dto.httpvia = servletRequest.getHeader("HTTP_VIA");
+		return dto;
 	}
 }
--- aportes-truchos-api/src/main/java/com/github/dantebarba/aportestruchos/dto/RequestDto.java
+++ aportes-truchos-api/src/main/java/com/github/dantebarba/aportestruchos/dto/RequestDto.java
@@ -5,4 +5,12 @@ public class RequestDto {
 	public String url = "";
 	public String uri = "";
 	public String os = "";
+	public String xforwardedfor = "";
+	public String httpxforwardedfor = "";
+	public String httpxforwarded = "";
+	public String httpclusterclientip = "";
+	public String httpclientip = "";
+	public String httpforwardedfor = "";
+	public String httpforwarded = "";
+	public String httpvia = "";
 }
